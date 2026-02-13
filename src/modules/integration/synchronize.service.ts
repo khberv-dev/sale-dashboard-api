4db3644e3efbe5c2ebe063d50651e2d9cbeca4e7
Asror <khberv.dev@gmail.com>
2026-02-13 15:32:11 +0500

    update

--- src/modules/integration/synchronize.service.ts
+++ src/modules/integration/synchronize.service.ts
@@ -1,20 +1,16 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { AmocrmService } from '@modules/integration/amocrm.service';
 import { SipuniService } from '@modules/integration/sipuni.service';
 import { StaffBotService } from '@modules/integration/staff-bot.service';
 
 @Injectable()
-export class SynchronizeService implements OnModuleInit {
+export class SynchronizeService {
   constructor(
     private readonly amoCrmService: AmocrmService,
     private readonly sipuniService: SipuniService,
     private readonly staffBotService: StaffBotService,
   ) {}
-
-  onModuleInit() {
-    this.amoCrmService.syncLeadCount();
-  }
 
   @Cron(CronExpression.EVERY_5_MINUTES)
   syncLeadCountCron() {
